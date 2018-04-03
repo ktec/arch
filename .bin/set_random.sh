bfabdd73f35ff32c2129e1e941353424acfc00a5
Keith Salisbury <keithsalisbury@gmail.com>
2018-04-03 06:11:27 +0100

    use local directory for wallpaper link

--- .bin/set_random.sh
+++ .bin/set_random.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE=`find /usr/share/wallpapers/ -type f | shuf -n1`
-LINK=/usr/share/wallpapers/wallpaper.jpg
+LINK=.local/share/wallpaper.jpg
 
 [[ -f $LINK ]] && rm -rf $LINK
 ln -s $SOURCE $LINK
